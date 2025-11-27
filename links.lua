1b48414bc4fda15fa44b617f34e78f702bb2d57a
zidiu5 <147642178+zidiu5@users.noreply.github.com>
2025-11-27 22:59:14 +0100

    Update links.lua

--- links.lua
+++ links.lua
@@ -1,1 +1,5 @@
-
+return {
+    {name="Autodelte", url="https://raw.githubusercontent.com/zidiu5/Pls-work/refs/heads/main/Autodelete.lua", gameId=0},
+    {name="PLS WORK", url="https://raw.githubusercontent.com/zidiu5/Pls-work/refs/heads/main/ok.lua", gameId=0},
+    {name="Test Pls Work", url="https://raw.githubusercontent.com/zidiu5/Pls-work/refs/heads/main/test-pls-work.lua", gameId=0}
+}
