6732aa1e88713007bda7563d4c2c560b6d0dd041
zidiu5 <147642178+zidiu5@users.noreply.github.com>
2025-11-27 23:14:45 +0100

    Update links.lua

--- links.lua
+++ links.lua
@@ -1,5 +1,5 @@
 return {
-    {name="Autodelte", url="https://raw.githubusercontent.com/zidiu5/Pls-work/refs/heads/main/Autodelete.lua", gameId=0},
-    {name="PLS WORK", url="https://raw.githubusercontent.com/zidiu5/Pls-work/refs/heads/main/ok.lua", gameId=168556275},
-    {name="Test Pls Work", url="https://raw.githubusercontent.com/zidiu5/Pls-work/refs/heads/main/test-pls-work.lua", gameId=0}
+    {name="Autodelete", url="https://raw.githubusercontent.com/zidiu5/Pls-work/refs/heads/main/Autodelete.lua", gameId=0, desc="Automatically deletes selected Pet Versions"},
+    {name="PLS WORK", url="https://raw.githubusercontent.com/zidiu5/Pls-work/refs/heads/main/ok.lua", gameId=168556275, desc="Main GUI for Mystery Games"},
+    {name="Test Pls Work", url="https://raw.githubusercontent.com/zidiu5/Pls-work/refs/heads/main/test-pls-work.lua", gameId=0, desc="Test Script"}
 }
