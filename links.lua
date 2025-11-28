522b8baee2c1cdb064876e8c4668283165e0826c
zidiu5 <147642178+zidiu5@users.noreply.github.com>
2025-11-28 21:01:03 +0100

    Update links.lua

--- links.lua
+++ links.lua
@@ -1,8 +1,8 @@
--- V1
+-- V1.1
 return {
     {name="Autodelete", url="https://raw.githubusercontent.com/zidiu5/Pls-work/refs/heads/main/Autodelete.lua", gameId={108900455260873}, desc="Auto deletes Pet versions"},
     {name="PLS WORK", url="https://raw.githubusercontent.com/zidiu5/Pls-work/refs/heads/main/ok.lua", gameId={168556275,108900455260873}, desc="Main GUI for Mystery Games"},
     {name="Test Pls Work", url="https://raw.githubusercontent.com/zidiu5/Pls-work/refs/heads/main/test-pls-work.lua", gameId={108900455260873}, desc="Test Script + Index Farm"},
     {name="WhiteGUI", url="https://raw.githubusercontent.com/zidiu5/TheStupidPSXCopy/refs/heads/main/IDK", gameId={108900455260873}, desc="Not optimized"},
-    {name="LoS", url="https://pastebin.com/raw/bs8aRU3Y", gameId=0, desc="Legends of Speed, prob on german"}
+    {name="LoS", url="https://pastebin.com/raw/bs8aRU3Y", gameId={3101667897}, desc="Legends of Speed, prob on german"}
 }
