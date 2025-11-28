50152ae0ad4471d723b0031e1b7f0cf1364cc45d
zidiu5 <147642178+zidiu5@users.noreply.github.com>
2025-11-28 17:40:25 +0100

    Update links.lua

--- links.lua
+++ links.lua
@@ -1,7 +1,7 @@
 return {
-    {name="Autodelete", url="https://raw.githubusercontent.com/zidiu5/Pls-work/refs/heads/main/Autodelete.lua", gameId=108900455260873, desc="Auto deletes Pet versions"},
+    {name="Autodelete", url="https://raw.githubusercontent.com/zidiu5/Pls-work/refs/heads/main/Autodelete.lua", gameId= {108900455260873[, desc="Auto deletes Pet versions"},
     {name="PLS WORK", url="https://raw.githubusercontent.com/zidiu5/Pls-work/refs/heads/main/ok.lua", gameId= {168556275, 108900455260873}, desc="Main GUI for Mystery Games"},
-    {name="Test Pls Work", url="https://raw.githubusercontent.com/zidiu5/Pls-work/refs/heads/main/test-pls-work.lua", gameId=108900455260873, desc="Test Script for PSX OG"}
-    {name="WhiteGUI", url="https://raw.githubusercontent.com/zidiu5/TheStupidPSXCopy/refs/heads/main/IDK", gameId=108900455260873, desc="Not optimized"}
+    {name="Test Pls Work", url="https://raw.githubusercontent.com/zidiu5/Pls-work/refs/heads/main/test-pls-work.lua", gameId= {108900455260873}, desc="Test Script for PSX OG"}
+    {name="WhiteGUI", url="https://raw.githubusercontent.com/zidiu5/TheStupidPSXCopy/refs/heads/main/IDK", gameId= {108900455260873}, desc="Not optimized"}
     {name="LoS", url="https://pastebin.com/raw/bs8aRU3Y", gameId=0, desc="Legends of Speed, prob on german"}
 }
