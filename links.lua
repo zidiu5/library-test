a72c1e60c3fc375e14b8174fca769e9391075789
zidiu5 <147642178+zidiu5@users.noreply.github.com>
2025-11-28 22:01:02 +0100

    Update links.lua

--- links.lua
+++ links.lua
@@ -4,5 +4,6 @@ return {
     {name="PLS WORK", url="https://raw.githubusercontent.com/zidiu5/Pls-work/refs/heads/main/ok.lua", gameId={168556275,108900455260873}, desc="Main GUI for Mystery Games"},
     {name="Test Pls Work", url="https://raw.githubusercontent.com/zidiu5/Pls-work/refs/heads/main/test-pls-work.lua", gameId={108900455260873}, desc="Test Script + Index Farm"},
     {name="WhiteGUI", url="https://raw.githubusercontent.com/zidiu5/TheStupidPSXCopy/refs/heads/main/IDK", gameId={108900455260873}, desc="Not optimized"},
-    {name="LoS", url="https://pastebin.com/raw/bs8aRU3Y", gameId={3101667897}, desc="Legends of Speed, prob on german"}
+    {name="LoS", url="https://pastebin.com/raw/bs8aRU3Y", gameId={3101667897}, desc="Legends of Speed, prob on german"},
+    {name="Prospecting", url="https://raw.githubusercontent.com/zidiu5/Pls-work/refs/heads/main/Prospecting.lua", gameId={129827112113663}, desc="Prospecting (Fluent GUI)"}
 }
